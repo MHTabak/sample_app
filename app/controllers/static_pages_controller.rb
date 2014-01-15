a5f82d24d09cc826106212344f5cb104c0ada1b3
MHTabak <michael_tabak@yahoo.com>
2014-01-15 15:30:01 -0800

    Finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+  
+  # Action for the home page
   def home
   end
 
+  # Action for the help page
   def help
   end
+
+  # Action for the About page
+  def about
+  end
+  
 end
